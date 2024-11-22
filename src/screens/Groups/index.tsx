b9ad73ce5ede69fbbf5f04f50077e5c7cf89d5f7
brunovalu16 <brunodesigner3@gmail.com>
2024-11-22 17:41:23 -0300

    criacao da pasta src

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export function Groups() {
+    return(
+        <View style={styles.container}>
+            <Text>Groups</Text>
+        </View>
+    )
+}
+
